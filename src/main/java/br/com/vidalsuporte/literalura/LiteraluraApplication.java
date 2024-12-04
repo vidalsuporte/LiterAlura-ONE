b3a9146b9b0a2fe7be2d781abc5d3f3eebc93a41
vidalsuporte <vidalsuporte@gmail.com>
2024-12-03 22:42:37 -0300

    Criação do projeto e organização dos pacotes

---
+++ src/main/java/br/com/vidalsuporte/literalura/LiteraluraApplication.java
@@ -0,0 +1,22 @@
+package br.com.vidalsuporte.literalura;
+
+import br.com.vidalsuporte.literalura.principal.Principal;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraluraApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteraluraApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Principal principal = new Principal();
+
+
+	}
+}
---
+++ src/main/java/br/com/vidalsuporte/literalura/principal/Principal.java
@@ -0,0 +1,23 @@
+package br.com.vidalsuporte.literalura.principal;
+
+public class Principal {
+
+
+
+    public void exibirMenu(){
+        var menu = """
+                Bem Vindo ao LiterAlura
+                Catálogo de livros nacionais e internacionais
+                
+                    """;
+
+
+
+    }
+
+
+
+
+
+
+}
